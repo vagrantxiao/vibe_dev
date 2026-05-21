2d2e2729f1d19584ae89a13e9b3d46c4c0892d7c
ylxiao <yuanlongxiao24@gmail.com>
2026-05-21 10:39:12 -0700

    Thu May 21 10:39:12 AM PDT 2026

--- async_fifo.sv
+++ async_fifo.sv
@@ -15,7 +15,35 @@ module async_fifo#(
     , output                 empty
 );
 
+  // -------------------------------------------------------
+  // Dual-port memory
+  // -------------------------------------------------------
+  reg [DATAWIDTH-1:0] mem [0:2**ADDRWIDTH-1];
 
+  // -------------------------------------------------------
+  // Write pointer (ADDRWIDTH+1 bits, MSB = wrap bit)
+  // -------------------------------------------------------
+  reg [ADDRWIDTH:0] wptr;
+
+  always @(posedge wclk or negedge wrst_n) begin
+    if (!wrst_n)
+      wptr <= '0;
+    else if (wrreq && !full)
+      wptr <= wptr + 1'b1;
+  end
+
+  // Write memory on every accepted write
+  always @(posedge wclk) begin
+    if (wrreq && !full)
+      mem[wptr[ADDRWIDTH-1:0]] <= data;
+  end
+
+  // -------------------------------------------------------
+  // Stubs (replaced in later milestones)
+  // -------------------------------------------------------
+  assign full  = 1'b0;
+  assign empty = 1'b1;
+  assign q     = '0;
 
 endmodule
         
--- run.tcl
+++ run.tcl
@@ -1,8 +1,6 @@
 create_project project_1 project_1 -part xczu9eg-ffvb1156-2-e -force
 set_property board_part xilinx.com:zcu102:part0:3.4 [current_project]
 add_files ../async_fifo.sv
-add_files ../asynchronizer.sv
-add_files ../ydff.sv
 add_files -fileset sim_1 ../tb_top.sv
 update_compile_order -fileset sim_1
 set_property top tb_top [get_filesets sim_1]
